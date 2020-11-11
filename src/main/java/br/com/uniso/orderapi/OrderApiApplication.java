548a64cc740f7be2e56f6c1728a749d6e9d07781
Marco Souza <marco.souza@mercadolivre.com>
2020-11-10 23:45:21 -0300

    Configuring Eureka

--- src/main/java/br/com/uniso/orderapi/OrderApiApplication.java
+++ src/main/java/br/com/uniso/orderapi/OrderApiApplication.java
@@ -2,7 +2,6 @@ package br.com.uniso.orderapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -11,7 +10,6 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 public class OrderApiApplication {
 
-	@LoadBalanced
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
